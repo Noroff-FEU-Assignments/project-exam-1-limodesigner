5d2d578a55869596e4393fa66df7f559e853a460
limodesigner <linda@digitaldesigner.no>
2023-09-03 14:17:08 +0200

    subscribe modal tmp

---
+++ js/components/subscribeModal.js
@@ -0,0 +1,12 @@
+// newsletter modal popup - footer: gjøre den automatisk til index.html! 3 sec.
+const modal = document.querySelector("#modal");
+const openModal = document.querySelector(".open-button");
+const closeModal = document.querySelector(".close-button");
+
+openModal.addEventListener("click", () => {
+  modal.showModal();
+});
+
+closeModal.addEventListener("click", () => {
+  modal.close();
+});
