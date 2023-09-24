b24c3d81e614361efef27009e5aa36f592efb690
limodesigner <linda@digitaldesigner.no>
2023-09-24 13:35:31 +0200

    update 1335

--- js/main.js
+++ js/main.js
@@ -28,4 +28,64 @@ export async function fetchBlogPosts(start = 0, perPage = 10) {
   }
 }
 
+// Function to fetch and display specific blog posts in cards
+async function fetchBlogPostById(postId) {
+  try {
+    const response = await fetch(`${apiPosts}/${postId}?_embed`);
+    if (!response.ok) {
+      throw new Error(
+        `Failed to fetch blog post: ${response.status} - ${response.statusText}`
+      );
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+}
+
+async function displayBlogCard(postId, containerId) {
+  const post = await fetchBlogPostById(postId);
+  if (post) {
+    const container = document.getElementById(containerId);
+    container.innerHTML = `
+          <div class="blog-card-content">
+              <h2>${post.title.rendered}</h2>
+              <p>${new Date(post.date).toLocaleDateString()}</p>
+              <p>${post.excerpt.rendered}</p>
+              <a class="read-more-button" href="blog.html?id=${
+                post.id
+              }">Read more</a>
+          </div>
+      `;
+    if (post._embedded && post._embedded["wp:featuredmedia"]) {
+      const featuredMedia = post._embedded["wp:featuredmedia"][0];
+      if (featuredMedia && featuredMedia.source_url) {
+        container.innerHTML = `
+                  <div class="blog-card-content">
+                      <h2>${post.title.rendered}</h2>
+                      <p class="reduce">${new Date(
+                        post.date
+                      ).toLocaleDateString()}</p>
+                      <p class="no__padding">${post.excerpt.rendered}</p>
+                      <a class="read-more-button" href="blog.html?id=${
+                        post.id
+                      }">Read >>></a>
+                  </div>
+                  <div class="blog-card-image">
+                      <img src="${featuredMedia.source_url}" alt="${
+          post.title.rendered
+        }">
+                  </div>
+              `;
+      }
+    }
+  }
+}
+
+// Display specific blog posts in cards
+displayBlogCard(332, "blogCard332");
+displayBlogCard(355, "blogCard355");
+
 updateCopyrightYear();
