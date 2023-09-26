996033fdb925243a3359659581ac40ffb23fd77e
limodesigner <linda@digitaldesigner.no>
2023-09-26 15:51:01 +0200

    260923-1

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,4 +1,5 @@
 // @author Linda Moenstre 2023 - <linda@digitaldesigner.no>
+
 import { showLoader, hideLoader } from "./loader.js";
 import { fetchBlogPosts } from "./main.js";
 import { updateCopyrightYear } from "./currentyear.js";
@@ -23,11 +24,11 @@ async function displayBlogPosts() {
     for (const post of posts) {
       const postContainer = document.createElement("div");
       postContainer.classList.add("blog-post");
-
+    
       const titleElement = document.createElement("h2");
       titleElement.classList.add("post-title");
       titleElement.textContent = post.title.rendered;
-
+    
       if (post._embedded && post._embedded["wp:featuredmedia"]) {
         const imageUrl = post._embedded["wp:featuredmedia"][0].source_url;
         const imageElement = document.createElement("img");
@@ -35,28 +36,32 @@ async function displayBlogPosts() {
         imageElement.src = imageUrl;
         postContainer.appendChild(imageElement);
       }
-
+    
       const descriptionElement = document.createElement("p");
       descriptionElement.classList.add("post-excerpt");
-
+    
       const apiContent = post.content.rendered;
       const parser = new DOMParser();
       const parsedContent = parser.parseFromString(apiContent, "text/html");
       const truncatedExcerpt = parsedContent.body.textContent.substring(0, 200);
-
+    
       descriptionElement.textContent = truncatedExcerpt + "...";
-
+    
+      const buttonContainer = document.createElement("div");
+      buttonContainer.classList.add("blog-button-container");
+    
       const readMoreButton = document.createElement("a");
-      readMoreButton.classList.add("blog-button");
+      readMoreButton.classList.add("blog-button", "right-align"); // Add both classes here
       readMoreButton.textContent = "Read more";
       readMoreButton.href = `blog.html?id=${post.id}`;
-
+    
       const readMoreParagraph = document.createElement("p");
       readMoreParagraph.appendChild(readMoreButton);
-
+    
+      buttonContainer.appendChild(readMoreParagraph);
       postContainer.appendChild(titleElement);
       postContainer.appendChild(descriptionElement);
-      postContainer.appendChild(readMoreParagraph);
+      postContainer.appendChild(buttonContainer); // Place the button container inside the post container
       blogPostsContainer.appendChild(postContainer);
     }
   } catch (error) {
--- js/blogpost.js
+++ js/blogpost.js
@@ -4,9 +4,8 @@ import { showLoader, hideLoader } from "./loader.js";
 import { updateCopyrightYear } from "./currentyear.js";
 
 document.addEventListener("DOMContentLoaded", function () {
-  const loader = document.querySelector(".loader");
   const postTitle = document.querySelector("#blog-header__title");
-  const postDateAuthor = document.querySelector(".post-date");
+  const postDate = document.querySelector(".post-date");
   const postMedia = document.querySelector(".post-media");
   const postText = document.querySelector(".post-text");
 
@@ -21,11 +20,24 @@ document.addEventListener("DOMContentLoaded", function () {
   fetch(postUrl)
     .then((response) => response.json())
     .then((data) => {
+      document.title = "Sweetheart Embroidery | " + data.title.rendered;
       postTitle.textContent = data.title.rendered;
-      postDateAuthor.textContent = `Published on ${new Date(
+      postDate.textContent = `Published on ${new Date(
         data.date
       ).toLocaleDateString()}`;
-      postMedia.innerHTML = `<img src="${data._embedded["wp:featuredmedia"][0].source_url}" alt="Featured Image">`;
+
+      // Check if 'wp:featuredmedia' exists in the data
+      if (data._embedded && data._embedded["wp:featuredmedia"]) {
+        const imageInfo = data._embedded["wp:featuredmedia"][0];
+        if (imageInfo.source_url) {
+          const featuredImage = document.createElement("img");
+          featuredImage.classList.add("post-image");
+          featuredImage.setAttribute("src", imageInfo.source_url);
+          featuredImage.setAttribute("alt", "Featured Image");
+          postMedia.appendChild(featuredImage);
+        }
+      }
+
       postText.innerHTML = data.content.rendered;
 
       hideLoader();
@@ -36,6 +48,73 @@ document.addEventListener("DOMContentLoaded", function () {
       // Hide the loader in case of an error
       hideLoader();
     });
-});
 
-updateCopyrightYear();
+  // Function to fetch and display specific blog posts in cards
+  async function fetchBlogPostById(postId) {
+    try {
+      const response = await fetch(
+        `https://sweetheartembroidery.com/wp-json/wp/v2/posts/${postId}?_embed`
+      );
+      if (!response.ok) {
+        throw new Error(
+          `Failed to fetch blog post: ${response.status} - ${response.statusText}`
+        );
+      }
+      const data = await response.json();
+      return data;
+    } catch (error) {
+      console.error(error);
+      return null;
+    }
+  }
+
+  async function displayBlogCard(postId, containerId) {
+    const post = await fetchBlogPostById(postId);
+    if (post) {
+      const container = document.getElementById(containerId);
+
+      // Create a new div element for the card
+      const cardDiv = document.createElement("div");
+      cardDiv.classList.add(".blog-card-background");
+
+      // Check if featured media exists and add the image
+      if (post._embedded && post._embedded["wp:featuredmedia"]) {
+        const featuredMedia = post._embedded["wp:featuredmedia"][0];
+        if (featuredMedia && featuredMedia.source_url) {
+          const imageDiv = document.createElement("div");
+          imageDiv.classList.add("blog-card-image");
+          const image = document.createElement("img");
+          image.src = featuredMedia.source_url;
+          image.alt = post.title.rendered;
+          imageDiv.appendChild(image);
+          cardDiv.appendChild(imageDiv);
+        }
+      }
+
+      // Create a div for the card content
+      const contentDiv = document.createElement("div");
+      contentDiv.classList.add("blog-card-content");
+
+      // Populate the card content
+      contentDiv.innerHTML = `
+      <h2>${post.title.rendered}</h2>
+      <p>${new Date(post.date).toLocaleDateString()}</p>
+      <p>${post.excerpt.rendered}</p>
+      <a class="blog-button flex" href="blog.html?id=${post.id}">Read more</a>
+    `;
+
+      // Append the content div to the card div
+      cardDiv.appendChild(contentDiv);
+
+      // Append the card div to the specified container
+      container.appendChild(cardDiv);
+    }
+  }
+
+  // Display specific blog posts in cards
+  displayBlogCard(340, "blogCard340");
+  displayBlogCard(350, "blogCard350");
+  displayBlogCard(358, "blogCard358");
+
+  updateCopyrightYear();
+});
