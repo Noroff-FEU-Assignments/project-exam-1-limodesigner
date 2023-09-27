6e93628cd7dada67be2f97fa61ab617d07a33cfb
limodesigner <linda@digitaldesigner.no>
2023-09-27 14:10:51 +0200

    Update blogpost.js

--- js/blogpost.js
+++ js/blogpost.js
@@ -3,69 +3,63 @@
 import { showLoader, hideLoader } from "./loader.js";
 import { updateCopyrightYear } from "./currentyear.js";
 
-document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", function () {
   const queryString = document.location.search;
   const params = new URLSearchParams(queryString);
   const id = params.get("id");
-  const blogContent = document.querySelector(".blog-content");
+  const blogPostContent = document.querySelector(".blog-content");
+
   const postUrl = `https://sweetheartembroidery.com/wp-json/wp/v2/posts/${id}?_embed`;
 
   showLoader();
 
-  try {
-    const response = await fetch(postUrl);
-    if (!response.ok) {
-      throw new Error(
-        `Failed to fetch blog post: ${response.status} - ${response.statusText}`
-      );
-    }
-    const data = await response.json();
+  fetch(postUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      //fetch doc title
+      document.title = "Sweetheart Embroidery | " + data.title.rendered;
 
-    // Create and append the title
-    const titleElement = document.createElement("h2");
-    titleElement.classList.add("blog-header__title");
-    titleElement.textContent = data.title.rendered;
-    blogContent.appendChild(titleElement);
-
-    // Create and append the publication date
-    const dateElement = document.createElement("p");
-    dateElement.textContent = `Published on ${new Date(
-      data.date
-    ).toLocaleDateString()}`;
-    blogContent.appendChild(dateElement);
-
-    // Check if featured media exists and add the image
-    if (data._embedded && data._embedded["wp:featuredmedia"]) {
-      const showImage = data._embedded["wp:featuredmedia"][0];
-      if (showImage.source_url) {
-        const featuredImage = document.createElement("img");
-        featuredImage.classList.add("post-image");
-        featuredImage.setAttribute("src", showImage.source_url);
-        featuredImage.setAttribute("alt", "Featured Image");
-        blogContent.appendChild(featuredImage);
-
-        // Add a click event listener to open the modal
-        featuredImage.addEventListener("click", () => {
-          openModal(showImage.source_url);
-        });
-      }
-    }
-
-    // Set document title
-    document.title = `Sweetheart Embroidery | ${data.title.rendered}`;
-
-    // Create and append the rest of the blog content
-    const contentRendered = `
+      const contentRendered = `
+      <h2 class="blog-header__title">${data.title.rendered}</h2>
       <div class="blog-images">${data.content.rendered}</div>
-    `;
+      <p>Published on ${new Date(data.date).toLocaleDateString()}</p>
+      `;
 
-    blogContent.innerHTML += contentRendered;
+      if (data._embedded && data._embedded["wp:featuredmedia"]) {
+        const showImage = data._embedded["wp:featuredmedia"][0];
+        if (showImage.source_url) {
+          const featuredImage = document.createElement("img");
+          featuredImage.classList.add("post-image");
+          featuredImage.setAttribute("src", showImage.source_url);
+          featuredImage.setAttribute("alt", "Featured Image");
+          postMedia.appendChild(featuredImage);
+        }
+      }
 
-    hideLoader();
-  } catch (error) {
-    console.error("Error fetching blog post", error);
-    hideLoader();
-  }
+      hideLoader();
+
+      contentRendered.innerHTML = blogPostContent;
+
+      const modalContainer = document.querySelector(".image-modal__page");
+      const modalImage = document.querySelector(".image-modal");
+
+      const showImage = () => {
+        modalContainer.style.display = "block";
+      };
+
+      modalImage.onclick = showImage;
+
+      modalContainer.onclick = function (event) {
+        if (event.target == modalContainer) {
+          modalContainer.style.display = "none";
+        }
+      };
+    })
+    .catch((error) => {
+      console.error("Error fetching blogpost", error);
+
+      hideLoader();
+    });
 
   // Extra posts for interest
   async function fetchBlogPostById(postId) {
@@ -90,6 +84,13 @@ document.addEventListener("DOMContentLoaded", async function () {
     const post = await fetchBlogPostById(postId);
     if (post) {
       const container = document.getElementById(containerId);
+
+      const figures = document.querySelectorAll("figure");
+      figures.forEach((figure) => {
+        figure.addEventListener("click", (event) => {
+          showImageModal(figure.querySelector("img"));
+        });
+      });
 
       const cardDiv = document.createElement("div");
       cardDiv.classList.add("blog-card-background");
@@ -130,26 +131,3 @@ document.addEventListener("DOMContentLoaded", async function () {
 
   updateCopyrightYear();
 });
-
-// Modal Functionality
-function openModal(imageSrc) {
-  const modal = document.createElement("div");
-  modal.classList.add("modal");
-  modal.innerHTML = `
-    <div class="modal-content">
-      <span class="close-modal">&times;</span>
-      <img src="${imageSrc}" alt="Featured Image">
-    </div>
-  `;
-
-  document.body.appendChild(modal);
-
-  const closeModalButton = modal.querySelector(".close-modal");
-  closeModalButton.addEventListener("click", () => {
-    closeModal(modal);
-  });
-}
-
-function closeModal(modal) {
-  modal.remove();
-}
